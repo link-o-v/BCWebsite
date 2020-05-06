f02c95d2f30eb741e8ac2294dc89f02b27d7347a
link-o-v <56926349+link-o-v@users.noreply.github.com>
2020-05-06 21:37:38 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,23 @@
+// Get the modal
+let modal = document.getElementById('popup');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+/*function myFunction(x) {
+    x.classList.toggle("change");
+}*/
+/*function myFunction() {
+    document.getElementById("mob-menu").classList.toggle("show");
+}*/
+
+/*function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}*/
---
+++ js/mob-menu.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+function aboutMe() {
+    document.getElementById("about").style.display = "block";
+}
